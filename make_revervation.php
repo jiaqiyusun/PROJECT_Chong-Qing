4c7d7eca5bcaf7947df4ef402dc062d463694f78
ariclinis <ariclinis1@gmail.com>
2020-08-20 22:37:05 +0100

    send Email 90%

--- make_revervation.php
+++ make_revervation.php
@@ -1,12 +1,40 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-ini_set( 'display_errors', 1 );
-error_reporting( E_ALL );
-$from = "ariclinis1@gmail.com";
-$to = "mikejiaqipt@gmail.com";
-$subject = "Checking PHP mail";
-$message = "PHP mail works just fine";
-$headers = "From:" . $from;
-mail($to,$subject,$message, $headers);
-echo "The email message was sent.";
-?> 
+require_once('src/PHPMailer.php');
+require_once('src/SMTP.php');
+require_once('src/Exception.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
+try {
+	$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+	$mail->isSMTP(True);
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = true;
+	$mail->Username = 'ariclinis1@gmail.com';
+    $mail->Password = '934641870Ariclinis1998';
+	$mail->Port = 587;
+
+	$mail->setFrom('ariclinis1@gmail.com');
+	$mail->addAddress('ariclinis1@gmail.com');
+
+	$mail->isHTML(true);
+	$mail->Subject = 'Teste de email via gmail Canal TI';
+	$mail->Body = 'Chegou o email teste do <strong>Canal TI</strong>';
+	$mail->AltBody = 'Chegou o email teste do Canal TI';
+
+	if($mail->send()) {
+		echo 'Email enviado com sucesso';
+	} else {
+		echo 'Email nao enviado';
+	}
+} catch (Exception $e) {
+	echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+}
