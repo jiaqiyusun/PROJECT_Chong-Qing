984ea4024507c1e45f476f5e818bc719de28bfbe
jiaqiyusun <jiaqi.yu.sun@gmail.com>
2020-08-23 21:14:37 +0100

    bebida change

--- menu.php
+++ menu.php
@@ -1915,7 +1915,7 @@ include $_SESSION['idioma'];
 								
 								<div class="col-md-12 col-lg-6 d-flex align-self-stretch">
 									<div class="menus d-sm-flex ftco-animate align-items-stretch">
-										<div class="menu-img img" style="background-image: url(images/v3.jpeg);"></div>
+										<div class="menu-img img" style="background-image: url(images/v3.jpg);"></div>
 										<div class="text d-flex align-items-center">
 														<div>
 											<div class="d-flex">
