89a27004125edf4c3a8318f2faef012f7921ce5b
jiaqiyusun <jiaqi.yu.sun@gmail.com>
2020-08-23 16:05:30 +0100

    chines change

--- contact.php
+++ contact.php
@@ -77,7 +77,7 @@ include $_SESSION['idioma'];
 	  </nav>
     <!-- END nav -->
  
-    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_3.jpg');" data-stellar-background-ratio="0.5">
+    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/alternatives/back.jpg');" data-stellar-background-ratio="0.5">
       
       <div class="overlay"></div>
 
