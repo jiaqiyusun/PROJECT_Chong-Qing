0079f514355e88ca30bdee401201f2b0ac108262
ariclinis <ariclinis1@gmail.com>
2020-08-26 18:16:49 +0100

    Same

--- index.php
+++ index.php
@@ -257,6 +257,7 @@ include $_SESSION['idioma'];
 
     <section class="ftco-section bg-light">
     	<div class="container">
+			
     		<div class="row no-gutters justify-content-center mb-5 pb-2">
           <div class="col-md-12 text-center heading-section ftco-animate">
           	<span class="subheading"><?php echo $trad['especiais']?></span>
